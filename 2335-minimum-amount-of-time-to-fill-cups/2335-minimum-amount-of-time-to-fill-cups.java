ca8d1098a5928ee94fbf8118fb9df1bf30cdb64d
Subhajit Bhakta <bhaktas484@gmail.com>
2026-08-26 21:01:08 +0530

    Time: 0 ms (100%), Space: 42.6 MB (98.32%) - LeetHub

---
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int fillCups(int[] amount) {
+       int max = 0, sum = 0;
+        for(int i: amount) {
+            max = Math.max(i, max);
+            sum += i;
+        }
+        return Math.max(max, (sum+1)  / 2);
+    }
+}
